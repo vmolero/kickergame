f9e7ad98ed78ecdf1e5a534824e10af595ff0ced
Victor Molero <victor.molero@gmail.com>
2017-04-04 17:33:35 +0200

    Setting up FOSUserBundle and creating basic forms

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,9 +13,29 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
     public function indexAction(Request $request)
+    {
+        return $this->redirect(rtrim($request->getRequestUri(), '/') . '/login');
+    }
+
+    /**
+     * @Route("/login_check", name="loginCheck")
+     */
+    public function loginCheck(Request $request)
+    {
+        die("check");
+        return $this->redirect($request->getRequestUri() . 'players/');
+    }
+
+
+
+    /**
+     * @Security("has_role('ROLE_USER')")
+     * @Route("/players/", name="showGames")
+     */
+    public function showGamesAction(Request $request)
     {
 
         return new Response('Default response');
@@ -24,18 +47,33 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/admin/login", name="adminLogin")
+     * @Method({"GET"})
+     * @Route("/admin/users/add", name="addUserForm")
      */
-    public function adminLoginAction(Request $request)
+    public function addUserFormAction(Request $request)
     {
-        return new Response('Admin Login');
+        return $this->render('Users/adduser.html.twig', []);
     }
 
     /**
-     * @Route("/player/login", name="playerLogin")
+     * @Method({"POST"})
+     * @Route("/admin/users/add", name="addUser")
      */
-    public function playerLoginAction(Request $request)
+    public function addUserAction(Request $request)
     {
-        return new Response('PLayer Login');
+        $username = $request->get('username');
+        $password = $request->get('password');
+        $email = $request->get('email');
+        $role = $request->get('role');
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPlainPassword($password);
+        $user->setRoles([$role == 1 ? 'ROLE_ADMIN' : 'ROLE_USER']);
+        $user->setEmail($email);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $this->addUserFormAction($request);
     }
 }
